7d6e82c3f702f29b59d5f3171636fd7cffc33c22
Eric Gustafson <edg@cs.ucsb.edu>
2019-11-22 10:24:03 -0800

    Fix a few scripts

--- test_samr21_http_plain.sh
+++ test_samr21_http_plain.sh
@@ -3,6 +3,6 @@
 BINARY=./tests/samr21_http/samr21_http_plain.yml
 INPUTS=./tests/samr21_http/inputs_plain
 OUTPUTS=./tests/samr21_http/output/
-HARNESS="python3 -m hal_fuzz.harness -c $BINARY"
-./afl-fuzz -t 500 -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
-#$HARNESS $INPUTS/input1
+HARNESS="python3 -m hal_fuzz.harness -d -t -c $BINARY"
+#./afl-fuzz -t 500 -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+$HARNESS $INPUTS/input1
--- test_stm32_udp_echo.sh
+++ test_stm32_udp_echo.sh
@@ -4,5 +4,5 @@ BINARY=./tests/stm32_udp_echo/stm32_udp_echo_server.yml
 INPUTS=./tests/stm32_udp_echo/inputs
 OUTPUTS=./tests/stm32_udp_echo/output/
 HARNESS="python -m hal_fuzz.harness -c $BINARY"
-./afl-fuzz -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
-#$HARNESS $INPUTS/UDP_Echo_Server_Client.pcapng.input
+#./afl-fuzz -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+$HARNESS $INPUTS/UDP_Echo_Server_Client.pcapng.input
