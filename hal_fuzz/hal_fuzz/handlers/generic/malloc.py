4b8982eb18f3fb712207cd9aaac8e2dbd1708515
Chaosdragon <2014253028@yonsei.ac.kr>
2020-11-29 23:55:49 -0800

    add Malloc function for memory bug detecting

--- hal_fuzz/hal_fuzz/handlers/generic/malloc.py
+++ hal_fuzz/hal_fuzz/handlers/generic/malloc.py
@@ -32,7 +32,7 @@ def _malloc(uc, size):
         base_addr = free_chunks[aligned_size].pop()
     else:
         base_addr = wilderness
-
+    print(size)
     result = _calc_retaddr(base_addr, size)
     
     allocated_chunks[result] = (base_addr, aligned_size)
@@ -42,6 +42,7 @@ def _malloc(uc, size):
         wilderness += aligned_size + PAGE_SIZE
     else:
         uc.mem_protect(base_addr, aligned_size, UC_PROT_READ | UC_PROT_WRITE)
+    uc.mem_write(base_addr, b'\xfa'*aligned_size)
 
     return result
 
@@ -51,13 +52,15 @@ def _free(uc, addr):
         crash()
 
     base_addr, aligned_size = allocated_chunks[addr]
+    uc.mem_write(base_addr, b'\xfd'*aligned_size)
+
     if aligned_size not in free_chunks:
         free_chunks[aligned_size] = [base_addr]
     else:
         free_chunks[aligned_size].append(base_addr)
-    del allocated_chunks[addr]
+    # del allocated_chunks[addr]
 
-    uc.mem_protect(base_addr, aligned_size, UC_PROT_NONE)
+    # uc.mem_protect(base_addr, aligned_size, UC_PROT_NONE)
 
 def _calloc(uc, size):
     res = _malloc(uc, size)
@@ -76,12 +79,12 @@ def _realloc(uc, addr, size):
     if aligned_size >= size:
         # reuse buffer
         tar_addr = _calc_retaddr(base_addr, size)
-        uc.mem_write(tar_addr, curr_contents)
+        uc.mem_write(tar_addr, bytes(curr_contents))
     else:
         # need to allocate new one
         _free(uc, addr)
         tar_addr = _malloc(uc, size)
-        uc.mem_write(tar_addr, curr_contents)
+        uc.mem_write(tar_addr, bytes(curr_contents))
     
     return tar_addr
 
@@ -104,13 +107,12 @@ def realloc(uc):
     res = _realloc(uc, addr, size)
     uc.reg_write(UC_ARM_REG_R0, res)
 
-
 def malloc(uc):
     size = uc.reg_read(UC_ARM_REG_R0)
     res = _malloc(uc, size)
     uc.reg_write(UC_ARM_REG_R0, res)
     print("malloc. size=0x{:x} -> 0x{:x}".format(size, res))
-    
+
 def memp_free(uc):
     addr = uc.reg_read(UC_ARM_REG_R1)
     _free(uc, addr)
@@ -121,3 +123,68 @@ def mem_free(uc):
 def mem_malloc(uc):
     # TODO: alignment guarantees
     malloc(uc)
+
+
+def memcpy(uc):
+    dest = uc.reg_read(UC_ARM_REG_R0)
+    src = uc.reg_read(UC_ARM_REG_R1)
+    n = uc.reg_read(UC_ARM_REG_R2)
+    print("memcpy. size=0x{:x} : 0x{:x} -> 0x{:x}".format(n, src, dest))
+    res = _memcpy(uc, dest, src, n)
+    uc.reg_write(UC_ARM_REG_R0, res)
+
+def _memcpy(uc, dest, src, n):
+    if not dest or not src:
+        print("Invalid memory write, crashing")
+        print("Null pointer dereference (dest= 0x{:x}, src= 0x{:x})".format(dest, src))
+        crash()
+
+    base_addr , aligned_size = (0, 0)
+    for chunk in allocated_chunks:
+        addr, s = allocated_chunks[chunk]
+        # memcpy can be used as 'memcpy(buff+0x10, src, 0x10)'
+        # so find chunk that contain destination address
+        if addr <= dest and dest < addr + s:
+            base_addr = addr
+            aligned_size = s
+            break
+    
+    if not base_addr:
+        # memcpy from stack to stack
+        content = uc.mem_read(src, n)
+        uc.mem_write(dest, bytes(content))
+        return dest
+
+    curr_size = base_addr + aligned_size - dest
+    if curr_size < n:
+        print("Heap based overflow detected, crashing")
+        print("(dest buf size= 0x{:x}, length to copy= 0x{:x})".format(curr_size, n))
+        crash()
+    else:
+        content = uc.mem_read(src, n)
+        
+        if content == b'\xfa'*n:
+            print("Using not initialized memory detected, crashing")
+            crash()
+        elif content == b'\xfd'*n:
+            # maybe using freed memory is detected by harness.py
+            print("Use after free detected, crashing")
+            crash()
+
+        content = uc.mem_read(dest, n)
+        
+        if content == b'\xfd'*n:
+            # maybe using freed memory is detected by harness.py
+            print("Use after free detected, crashing")
+            crash()
+
+        uc.mem_write(dest, bytes(content))
+
+    return base_addr
+
+def func1(uc):
+    print('------- Hello Func1 -------')
+
+def vuln(uc):
+    print('-------- WARNING --------')
+    print('** Vulnerable function **')
